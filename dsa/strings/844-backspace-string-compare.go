02c64c46d613b8a07872064513b0a29d72c3679a
shafizod <shahzodshafizod@mail.ru>
2024-01-07 15:36:48 +0500

    strings: backspace string compare (easy)

---
+++ dsa/strings/844-backspace-string-compare.go
@@ -0,0 +1,112 @@
+package strings
+
+/*
+Problem:
+Given two strings S and T, return if they equal when both are typed out.
+Any '#' that appears in the string counts as a backspace
+
+Step 1: Verify the constraints
+	- What happens when two #'s appear beside each other?
+		: Delete the two values before the first #.
+	- What happens to # when there is no character to remove?
+		: It deletes nothing then, just like backspace would.
+	- Are two empty strings equal to each other?
+		: Yes, consider two empty strings as equal.
+	- Does case sensitivity matter?
+		: Yes it does, "a" does not equal "A".
+Step 2: Write out some test cases
+	- "ab#z", "az#z" -> true
+	- "abc#d", "acc#d" -> false
+	- "x#y#z#", "a#" -> true
+	- "a###b", "b" -> true
+	- "Ab#z", "ab#z" -> false
+Step 8: Can we optimize our solution?
+	- 1st Hint: Utilize the original strings.
+	- 2nd Hint: Use the 2 pointer technique.
+	- 3rd Hint: Start from the end of the strings!
+*/
+
+// https://leetcode.com/problems/backspace-string-compare/
+
+func backspaceCompare(s string, t string) bool {
+	var equal = true
+	var skips int
+	for indexS, indexT := len(s)-1, len(t)-1; ; {
+		for indexS >= 0 && s[indexS] == '#' {
+			skips = 0
+			for indexS >= 0 && s[indexS] == '#' {
+				indexS--
+				skips++
+			}
+			for skips > 0 && indexS >= 0 {
+				if s[indexS] == '#' {
+					skips += 2
+				}
+				indexS--
+				skips--
+			}
+		}
+
+		for indexT >= 0 && t[indexT] == '#' {
+			skips = 0
+			for indexT >= 0 && t[indexT] == '#' {
+				indexT--
+				skips++
+			}
+			for skips > 0 && indexT >= 0 {
+				if t[indexT] == '#' {
+					skips += 2
+				}
+				indexT--
+				skips--
+			}
+		}
+
+		if indexS < 0 && indexT < 0 {
+			break
+		}
+
+		if indexS < 0 || indexT < 0 {
+			equal = false
+			break
+		}
+
+		if s[indexS] != t[indexT] {
+			equal = false
+			break
+		}
+
+		indexS--
+		indexT--
+	}
+	return equal
+}
+
+// func backspaceCompare(s string, t string) bool {
+// 	runeS := removeHashs(s)
+// 	runeT := removeHashs(t)
+// 	if len(runeS) != len(runeT) {
+// 		return false
+// 	}
+// 	for i := len(runeS) - 1; i >= 0; i-- {
+// 		if runeS[i] != runeT[i] {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// func removeHashs(str string) []rune {
+// 	var result = make([]rune, 0)
+// 	_ = strings.Map(func(r rune) rune {
+// 		if r != '#' {
+// 			result = append(result, r)
+// 		} else {
+// 			if length := len(result); length > 0 {
+// 				result = result[:length-1]
+// 			}
+// 		}
+// 		return r
+// 	}, str)
+// 	return result
+// }
---
+++ dsa/strings/844-backspace-string-compare_test.go
@@ -0,0 +1,33 @@
+package strings
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v -count=1 ./dsa/strings/ -run ^TestBackspaceCompare$
+func TestBackspaceCompare(t *testing.T) {
+	for _, data := range []struct {
+		s     string
+		t     string
+		equal bool
+	}{
+		{s: "a###b", t: "", equal: false},
+		{s: "ab#z", t: "az#z", equal: true},
+		{s: "abc#d", t: "acc#d", equal: false},
+		{s: "x#y#z#", t: "a#", equal: true},
+		{s: "ax#y#z#", t: "a", equal: true},
+		{s: "a###b", t: "b", equal: true},
+		{s: "Ab#z", t: "ab#z", equal: false},
+		{s: "abc", t: "abc", equal: true},
+		{s: "a########", t: "", equal: true},
+		{s: "ab#c", t: "ad#c", equal: true},
+		{s: "ab##", t: "c#d#", equal: true},
+		{s: "a#c", t: "b", equal: false},
+		{s: "bxj##tw", t: "bxo#j##tw", equal: true},
+	} {
+		equal := backspaceCompare(data.s, data.t)
+		assert.Equal(t, data.equal, equal)
+	}
+}
