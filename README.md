89e59a6de7abd2b05f7bab245eefc706f790cf00
shahzodshafizod <shahzodshafizod@gmail.com>
2024-10-31 23:04:55 +0500

    dp: minimum total distance traveled (hard)

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 | #### | Title | Difficulty | Tags | Date | Solutions
 | ---- | ----- | ---------- | ---- | ---- | ---------
+| 2463 | [Minimum Total Distance Traveled](https://leetcode.com/problems/minimum-total-distance-traveled/) | Hard | Array, Dynamic Programming, Sorting | 2024-10-31 | [Go](dp/2463-minimum-total-distance-traveled.go) [Python](dp/2463-minimum-total-distance-traveled.py)
 | 1671 | [Minimum Number of Removals to Make Mountain Array](https://leetcode.com/problems/minimum-number-of-removals-to-make-mountain-array/) | Hard | Array, Binary Search, Dynamic Programming, Greedy | 2024-10-30 | [Go](dp/1671-minimum-number-of-removals-to-make-mountain-array.go) [Python](dp/1671-minimum-number-of-removals-to-make-mountain-array.py)
 | 1563 | [Stone Game V](https://leetcode.com/problems/stone-game-v/) | Hard | Array, Math, Dynamic Programming, Game Theory | 2024-10-29 | [Go](dp/1563-stone-game-v.go) [Python](dp/1563-stone-game-v.py)
 | 1510 | [Stone Game IV](https://leetcode.com/problems/stone-game-iv/) | Hard | Math, Dynamic Programming, Game Theory | 2024-10-28 | [Go](dp/1510-stone-game-iv.go) [Python](dp/1510-stone-game-iv.py)
---
+++ dp/2463-minimum-total-distance-traveled.go
@@ -0,0 +1,90 @@
+package dp
+
+import (
+	"sort"
+)
+
+// https://leetcode.com/problems/minimum-total-distance-traveled/
+
+// Approach: Bottom-Up Dynamic Programming
+// Time: O(RF), R=len(robot), F=sum(factory[i][1])
+// Space: O(RF)
+func minimumTotalDistance(robot []int, factory [][]int) int64 {
+	var abs = func(x int) int64 {
+		if x < 0 {
+			x = -x
+		}
+		return int64(x)
+	}
+	sort.Ints(robot)
+	sort.Slice(factory, func(i, j int) bool { return factory[i][0] < factory[j][0] })
+	var factories = make([]int, 0, len(factory))
+	for fid := range factory {
+		for limit := factory[fid][1]; limit > 0; limit-- {
+			factories = append(factories, factory[fid][0])
+		}
+	}
+	var rlen, flen = len(robot), len(factories)
+	var dp = make([][]int64, rlen+1)
+	for idx := range dp {
+		dp[idx] = make([]int64, flen+1)
+		dp[idx][flen] = 1e12
+	}
+	dp[rlen][flen] = 0
+	var distance int64
+	for rid := rlen - 1; rid >= 0; rid-- {
+		for fid := flen - 1; fid >= 0; fid-- {
+			distance = abs(robot[rid] - factories[fid])
+			dp[rid][fid] = min(
+				distance+dp[rid+1][fid+1], // include factory
+				dp[rid][fid+1],            // skip factory
+			)
+		}
+	}
+	return dp[0][0]
+}
+
+// // Approach: Top-Down Dynamic Programming
+// // Time: O(RF), R=len(robot), F=sum(factory[i][1])
+// // Space: O(RF)
+// func minimumTotalDistance(robot []int, factory [][]int) int64 {
+// 	var abs = func(x int) int64 {
+// 		if x < 0 {
+// 			x = -x
+// 		}
+// 		return int64(x)
+// 	}
+// 	sort.Ints(robot)
+// 	sort.Slice(factory, func(i, j int) bool { return factory[i][0] < factory[j][0] })
+// 	var factories = make([]int, 0, len(factory))
+// 	for fid := range factory {
+// 		for limit := factory[fid][1]; limit > 0; limit-- {
+// 			factories = append(factories, factory[fid][0])
+// 		}
+// 	}
+// 	var rlen, flen = len(robot), len(factories)
+// 	var memo = make([][]*int64, rlen)
+// 	for idx := range memo {
+// 		memo[idx] = make([]*int64, flen)
+// 	}
+// 	var dp func(rid int, fid int) int64
+// 	dp = func(rid int, fid int) int64 {
+// 		if rid == rlen {
+// 			return 0
+// 		}
+// 		if fid == flen {
+// 			return 1e12
+// 		}
+// 		if memo[rid][fid] != nil {
+// 			return *memo[rid][fid]
+// 		}
+// 		var distance = abs(robot[rid] - factories[fid])
+// 		memo[rid][fid] = new(int64)
+// 		*memo[rid][fid] = min(
+// 			distance+dp(rid+1, fid+1), // include factory
+// 			dp(rid, fid+1),            // skip factory
+// 		)
+// 		return *memo[rid][fid]
+// 	}
+// 	return dp(0, 0)
+// }
